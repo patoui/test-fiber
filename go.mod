7c06264b5abfdeb42d461e14ecd669363758f9bb
Patrique Ouimet <patrique.ouimet@gmail.com>
2020-11-16 15:01:45 -0500

    Added template dir and assets

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/gofiber/fiber/v2 v2.2.0
+	github.com/gofiber/template v1.6.4 // indirect
 	github.com/jinzhu/gorm v1.9.16
 )
--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/patoui/test-fiber/database"
 	"github.com/patoui/test-fiber/modules/book"
+	"github.com/patoui/test-fiber/modules/home"
 )
 
 func setupRoutes(app *fiber.App) {
+	// Assets
+	app.Static("/static", "./static")
+
+	// Views
+	app.Get("/", home.Index)
+
+	// API
 	app.Get("/api/v1/book", book.GetBooks)
 	app.Get("/api/v1/book/:id", book.GetBook)
 	app.Post("/api/v1/book", book.NewBook)
@@ -29,7 +38,10 @@ func initDatabase() {
 }
 
 func main() {
-	app := fiber.New()
+	engine := html.New("./templates", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 	initDatabase()
 	setupRoutes(app)
 	app.Listen(":3000")
